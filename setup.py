398fe69c458fe5c78c88b03297f426376ebd5e00
Yash Narang <narangyash31@gmail.com>
2024-12-24 12:08:06 +0530

    added data to the mongodb

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Yash",
+    author_email="narangyash31@gmail.com",
+    packages=find_packages()
+)
